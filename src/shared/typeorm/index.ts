c0175435da1f11ebf47b2b647f64043cae087b47
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-09 11:08:14 -0300

    Inicializacao TypeORM

---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1234567',
+    database: 'apivendas',
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log('PostGres inicializado com sucesso');
+    })
+    .catch(err => {
+        console.error('Error during Data Source initialization', err);
+    });
