8e39610d44cd88201e748f1ec7bf2e1e9cafe536
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-09 18:35:51 -0300

    Ajustado para typeorm 0.2.9

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,19 +1,3 @@
-import { DataSource } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '1234567',
-    database: 'apivendas',
-    migrations: ['./src/shared/typeorm/migrations/*.ts'],
-});
-
-AppDataSource.initialize()
-    .then(() => {
-        console.log('PostGres inicializado com sucesso');
-    })
-    .catch(err => {
-        console.error('Error during Data Source initialization', err);
-    });
+createConnection();
