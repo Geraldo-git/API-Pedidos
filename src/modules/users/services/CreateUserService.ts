dc853d88a88215fa85e9c221418d8eccf40940e5
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-17 11:10:00 -0300

    Classe CreateUserService

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+class CreateUserService {
+    public async execute({ name, email, password }: IRequest): Promise<User> {
+        const usersRepository = getCustomRepository(UserRepository);
+
+        const emailExists = await usersRepository.findByName(email);
+        if (emailExists) {
+            throw new AppError('email já está sendo utilizado!');
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            password,
+        });
+
+        await usersRepository.save(user);
+        return user;
+    }
+}
+
+export default CreateUserService;
